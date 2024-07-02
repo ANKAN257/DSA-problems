b7d2bc1b033b0f63fe43c33f9f619da88e099914
ankan kumar <ankankumar071@gmail.com>
2024-07-03 00:47:34 +0530

    implement_stack_using_Array

--- stack_&_queue/implement_queue_using_arrray.cpp
+++ stack_&_queue/implement_queue_using_arrray.cpp
@@ -1,69 +1,88 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define MAX_SIZE 100
-class class_queue{
-  int arr[MAX_SIZE];
-  int frontIdx;
-  int rearIdx;
-  public:
-   void push_fun(int val);
-   void pop_fun();
-   int top_fun();
-   bool isEmpty_fun();
-   int size_fun();
-  
-//contructor
-class_queue(int size){
-    frontIdx=-1;
-    rearIdx = -1;
-}
+
+class Queue {
+    int arr[MAX_SIZE];
+    int frontIdx;
+    int rearIdx;
+public:
+    void push_fun(int val);
+    void pop_fun();
+    int top_fun();
+    bool isEmpty_fun();
+    int size_fun();
+
+    // Constructor
+    Queue() {
+        frontIdx = -1;
+        rearIdx = -1;
+    }
 };
 
-void class_queue::push_fun(int val){
-    if(rearIdx==MAX_SIZE-1){
-          cout<<"queue is full now!"<<endl;
-          return ;
-    }else kif(isEmpty_fun()){
-         frontIdx=0;
-         rearIdx=0;
+void Queue::push_fun(int val) {
+    if (rearIdx == MAX_SIZE - 1) {
+        cout << "Queue is full now!" << endl;
+        return;
+    } else if (isEmpty_fun()) {
+        frontIdx = 0;
+        rearIdx = 0;
+    } else {
+        rearIdx++;
     }
-    arr[rearIdx++]=val;
+    arr[rearIdx] = val;
 }
 
-bool class_queue::isEmpty_fun(){
-     return frontIdx=-1&&rearIdx==-1;
+bool Queue::isEmpty_fun() {
+    return frontIdx == -1 && rearIdx == -1;
 }
 
-void class_queue::pop_fun(){
-     if(isEmpty_fun()){
-        cout<<"Empty queue "<<endl;
-     }else if(frontIdx==rearIdx){
-         frontIdx = -1;
-         rearIdx  = -1;
-     }
-      frontIdx++;
+void Queue::pop_fun() {
+    if (isEmpty_fun()) {
+        cout << "Empty queue " << endl;
+        return;
+    } else if (frontIdx == rearIdx) {
+        frontIdx = -1;
+        rearIdx = -1;
+    } else {
+        frontIdx++;
+    }
 }
 
-int class_queue::top_fun(){
-     if(isEmpty_fun()){
-        cout<<"queue is empty"<<endl;
+int Queue::top_fun() {
+    if (isEmpty_fun()) {
+        cout << "Queue is empty" << endl;
         return -1;
-     }
-
-    return  arr[frontIdx];
+    }
+    return arr[frontIdx];
 }
 
-int class_queue::size_fun(){
-    if(isEmpty_fun) return 0;
-    return rearIdx-frontIdx+1;
+int Queue::size_fun() {
+    if (isEmpty_fun()) return 0;
+    return rearIdx - frontIdx + 1;
 }
 
+int main() {
+    Queue q;
 
-int main(){
+    q.push_fun(10);
+    q.push_fun(20);
+    q.push_fun(40);
+    q.push_fun(80);
+    q.push_fun(160);
 
+    cout << "Front or top of queue: " << q.top_fun() << endl;
 
+    q.pop_fun();
 
+    cout << "Top after pop queue: " << q.top_fun() << endl;
 
+    while (!q.isEmpty_fun()) {
+        cout << q.top_fun() << "  ";
+        q.pop_fun();
+    }
+    cout << endl;
 
+    return 0;
 }
--- stack_&_queue/implement_stack_using_Array.cpp
+++ stack_&_queue/implement_stack_using_Array.cpp
@@ -8,13 +8,6 @@ private:
     int topidx;
 
 public:
-    // Function declarations
-    void push_fun(int val);
-    void pop_fun();
-    int top_fun();
-    bool isEmpty_fun();
-    int size_fun();
-
     // Constructor
     Stack_class(int size) {
         arr = new int[size];
@@ -26,6 +19,13 @@ public:
     ~Stack_class() {
         delete[] arr;
     }
+
+    // Function declarations
+    void push_fun(int val);
+    void pop_fun();
+    int top_fun();
+    bool isEmpty_fun();
+    int size_fun();
 };
 
 // Push an element onto the stack
@@ -40,7 +40,7 @@ void Stack_class::push_fun(int val) {
 // Pop an element from the stack
 void Stack_class::pop_fun() {
     if (topidx == -1) {
-        cout << "Stack is empty" << endl;
+        cout << "Stack is empty  \n";
         return;
     }
     --topidx;
@@ -49,7 +49,7 @@ void Stack_class::pop_fun() {
 // Get the top element of the stack
 int Stack_class::top_fun() {
     if (topidx == -1) {
-        cout << "Stack is empty" << endl;
+        cout << "Stack is empty  " ;
         return -1;
     }
     return arr[topidx];
@@ -68,15 +68,26 @@ int Stack_class::size_fun() {
 // Usage example
 int main() {
     int n = 5;
-
     Stack_class st(n);
 
-    for (int i = 0; i < n; i++) {
-        int val;
-        cin >> val;
-        st.push_fun(val);
+    // Manually pushing elements onto the stack
+    st.push_fun(10);
+    st.push_fun(20);
+    st.push_fun(30);
+    st.push_fun(40);
+    st.push_fun(50);
+    cout << "Elements in the stack from top to bottom:" << endl;
+    while (!st.isEmpty_fun()) {
+        cout << st.top_fun() << " ";
+        st.pop_fun();
     }
+    cout << endl;
+    
+    cout<<"Before poping element"<<st.top_fun()<<endl;//stack is empty ,-1
+    st.pop_fun();//stack is empty
+    cout<<"after poping ele "<<st.top_fun()<<endl;//stack is empty ,-1
 
+    cout << "Elements in the stack from top to bottom:" << endl;
     while (!st.isEmpty_fun()) {
         cout << st.top_fun() << " ";
         st.pop_fun();
